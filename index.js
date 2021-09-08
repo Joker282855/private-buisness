7758a090b951d35d8073c0e2a6f746baa094b349
unknown <jtjackson325@gmail.com>
2021-09-08 12:16:02 -0600

    got the inital files set up for the HTML generation

---
+++ index.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generatePage = require('./src/generatePage');
+
+// Array that will hold the quesiton for the file generation
+
+// Create a function to write an HTML file
+
+// Create a function to initallize the HTML file
+
+// Function call to initallize app
+
