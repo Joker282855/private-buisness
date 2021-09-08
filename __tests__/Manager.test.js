9b08b9ccfdb710086f991f8ca1b50e21aa00d2cd
unknown <jtjackson325@gmail.com>
2021-09-08 10:17:05 -0600

    got the test and method for the manager set up correctyly

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,4 +1,16 @@
 const Manager = require('../lib/Manager.js');
-const Employee = require('../lib/Employee.js');
 
-jest.mock('../lib/Employee');
+test('get the officeNumber of the Manager', () => {
+    const manager = new Manager('officeNumber');
+
+    expect(manager.name).toBe('officeNumber');
+    expect(manager.value).toEqual(expect.any(Number));
+});
+
+test('return everything for Manager', () => {
+    const managerRoll = new Manager();
+
+    expect(managerRoll.getRoll()).toHaveProperty('officeNumber');
+});
+
+
