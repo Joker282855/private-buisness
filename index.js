968414bcee91fedecb9482b22bd90c8962da0a9c
unknown <jtjackson325@gmail.com>
2021-09-08 12:44:06 -0600

    got the first questions written for the HTML file being generated

--- index.js
+++ index.js
@@ -4,6 +4,28 @@ const generatePage = require('./src/generatePage');
 
 // Array that will hold the quesiton for the file generation
 
+const information = [
+    {
+        type: 'input',
+        name: 'manager',
+        message: 'What is the name of your team Manager' 
+    },
+    {
+        type: 'input',
+        name: 'employeeId',
+        message: 'what is your employee ID'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address'
+    },
+    {
+        type: 'input',
+        name: ''
+    }
+]
+
 // Create a function to write an HTML file
 
 // Create a function to initallize the HTML file
