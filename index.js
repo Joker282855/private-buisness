7340ff21bfef485b793dc0b4cbf89e4bf3765e46
unknown <jtjackson325@gmail.com>
2021-09-09 17:21:42 -0600

    got the inquirer part way more organized than I had before

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const { inherits } = require('util');
 const generatePage = require('./dist/generatePage');
 
 // Write the prompts for the different situations
 
-inquirer
-    .prompt([
+const promptManager = () => {
+    return inquirer.prompt([
         {
             type: 'input',
             name: 'manager',
@@ -32,8 +31,22 @@ inquirer
             name: 'menu',
             message: 'Look at the menu below and choose an option',
             choices: ['add engineer', 'add intern', 'finish building my team' ]
-        },
-    ]) 
+        }
+    ]);
+};
+
+const promptMenu = () => {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'menuTwo',
+            message: 'Look at the options below and chose an option',
+            choices: ['add engineer', 'add intern', 'finish building my team']
+        }
+    ])
+}
+
+promptManager()
     .then((answers) => {
         if (answers.menu === 'add engineer') {
             return inquirer
@@ -53,12 +66,6 @@ inquirer
                         name: 'github',
                         message: 'What is the github username for the '
                     },
-                    {
-                        type: 'list',
-                        name: 'menuTwo',
-                        message: 'Look at the menu below and choose an option',
-                        choices: ['add engineer', 'add intern', 'finish building my team']
-                    }
                 ]);
         }
         else if (answers.menu === 'add intern') {
@@ -83,13 +90,76 @@ inquirer
                         type: 'input',
                         name: 'internGithub',
                         message: 'What is the github username for your intern'
+                    }
+                ]);            
+        }
+        else {
+            return 'Goodluck with your team'
+        } 
+    })
+    .then(promptMenu)
+    .then(menuAnswers => {
+        if (menuAnswers.menuTwo === 'add engineer') {
+            return inquirer
+                .prompt([
+                    {
+                        type: 'input',
+                        name: 'manager',
+                        message: 'What is the name of your team manager' 
+                    },
+                    {
+                        type: 'input',
+                        name: 'employeeId',
+                        message: 'what is the employee ID of your team manager'
+                    },
+                    {
+                        type: 'input',
+                        name: 'email',
+                        message: 'What is the email address of your team manager'
+                    },
+                    {
+                        type: 'input',
+                        name: 'office',
+                        message: 'What is your office number of your team manager'
                     },
                     {
                         type: 'list',
-                        name: 'menuThree',
+                        name: 'menu',
                         message: 'Look at the menu below and choose an option',
-                        choices: ['add engineer', 'add intern', 'finish building my team']
+                        choices: ['add engineer', 'add intern', 'finish building my team' ]
+                    },
+                ])
+        }
+        else if (menuAnswers.menuTwo === 'add intern') {
+            return inquirer
+                .prompt([
+                    {
+                        type: 'input',
+                        name: 'internName',
+                        message: 'What is the name of your intern'
+                    },
+                    {
+                        type: 'input',
+                        name: 'internId',
+                        message: 'What is the ID number of your intern'
+                    },
+                    {
+                        type: 'input',
+                        name: 'internEmail',
+                        message: 'What is the email of your intern'
+                    },
+                    {
+                        type: 'input',
+                        name: 'internGithub',
+                        message: 'What is the github username for your intern'
+                    },
+                    {
+                        type: 'list',
+                        name: 'menu',
+                        message: 'Look at the menu below and choose an option',
+                        choices: ['add engineer', 'add intern', 'finish building my team' ]
                     }
-                ]);            
-        } 
-});
+                ])
+        }
+    })
+
