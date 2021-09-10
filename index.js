866274354f343031d8232eb76ac431d7cbb802c1
unknown <jtjackson325@gmail.com>
2021-09-10 12:32:15 -0600

    got the html file being generated and now have to work on the syling of the html page

--- index.js
+++ index.js
@@ -4,162 +4,109 @@ const generatePage = require('./dist/generatePage');
 
 // Write the prompts for the different situations
 
-const promptManager = () => {
-    return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'manager',
-            message: 'What is the name of your team manager' 
-        },
-        {
-            type: 'input',
-            name: 'employeeId',
-            message: 'what is the employee ID of your team manager'
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: 'What is the email address of your team manager'
-        },
-        {
-            type: 'input',
-            name: 'office',
-            message: 'What is your office number of your team manager'
-        },
-        {
-            type: 'list',
-            name: 'menu',
-            message: 'Look at the menu below and choose an option',
-            choices: ['add engineer', 'add intern', 'finish building my team' ]
-        }
-    ]);
-};
+const information = [{
+        type: 'input',
+        name: 'manager',
+        message: 'What is the name of your team manager' 
+    },
+    {
+        type: 'input',
+        name: 'employeeId',
+        message: 'what is the employee ID of your team manager'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is the email address of your team manager'
+    },
+    {
+        type: 'input',
+        name: 'office',
+        message: 'What is your office number of your team manager'
+    },
+    {
+        type: 'list',
+        name: 'menu',
+        message: 'Look at the menu below and choose an option',
+        choices: ['add engineer', 'add intern', 'finish building my team' ]
+    },
+    {
+        type: 'input',
+        name: 'engineerName',
+        message: 'what is the name of your engineer'
+    },
+    {
+        type: 'input',
+        name: 'engineerId',
+        message: 'What is the id of your engineer'
+    },
+    {
+        type: 'input',
+        name: 'engineerEmail',
+        message: 'What is the email of your engineer'
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is the github username of your engineer'
+    },
+    {
+        type: 'list',
+        name: 'menuTwo',
+        message: 'Look at the menu below and choose an option',
+        choices: ['add engineer', 'add intern', 'finish building my team' ]
+    },
+    {
+        type: 'input',
+        name: 'internName',
+        message: 'What is the name of your intern'
+    },
+    {
+        type: 'input',
+        name: 'internId',
+        message: 'What is the ID number of your intern'
+    },
+    {
+        type: 'input',
+        name: 'internEmail',
+        message: 'What is the email of your intern'
+    },
+    {
+        type: 'input',
+        name: 'internSchool',
+        message: 'Where did your intern attend school'
+    },
+    {
+        type: 'list',
+        name: 'menuThree',
+        message: 'Look at the menu below and choose an option',
+        choices: ['add engineer', 'add intern', 'finish building my team' ]
+    }
+]
 
-const promptMenu = () => {
-    return inquirer.prompt([
-        {
-            type: 'list',
-            name: 'menuTwo',
-            message: 'Look at the options below and chose an option',
-            choices: ['add engineer', 'add intern', 'finish building my team']
-        }
-    ])
+
+// create the function to write the html file
+function writeToFile(fileName, data) {
+    
+    fs.writeFile(fileName, data, function(err) {
+        console.log(fileName)
+        console.log(data)
+            if (err) {
+                return console.log(err)
+            } else {
+                return console.log('Got the index file created. Hope it rocks your world')
+            }
+    })
 }
 
-promptManager()
-    .then((answers) => {
-        if (answers.menu === 'add engineer') {
-            return inquirer
-                .prompt([
-                    {
-                        type: 'input',
-                        name: 'engineerName',
-                        message: 'what is the name of your engineer'
-                    },
-                    {
-                        type: 'input',
-                        name: 'engineerEmail',
-                        message: 'What is the email of your engineer'
-                    },
-                    {
-                        type: 'input',
-                        name: 'github',
-                        message: 'What is the github username for the '
-                    },
-                ]);
-        }
-        else if (answers.menu === 'add intern') {
-            return inquirer
-                .prompt([
-                    {
-                        type: 'input',
-                        name: 'internName',
-                        message: 'What is the name of your intern'
-                    },
-                    {
-                        type: 'input',
-                        name: 'internId',
-                        message: 'What is the ID number of your intern'
-                    },
-                    {
-                        type: 'input',
-                        name: 'internEmail',
-                        message: 'What is the email of your intern'
-                    },
-                    {
-                        type: 'input',
-                        name: 'internGithub',
-                        message: 'What is the github username for your intern'
-                    }
-                ]);            
-        }
-        else {
-            return 'Goodluck with your team'
-        } 
-    })
-    .then(promptMenu)
-    .then(menuAnswers => {
-        if (menuAnswers.menuTwo === 'add engineer') {
-            return inquirer
-                .prompt([
-                    {
-                        type: 'input',
-                        name: 'manager',
-                        message: 'What is the name of your team manager' 
-                    },
-                    {
-                        type: 'input',
-                        name: 'employeeId',
-                        message: 'what is the employee ID of your team manager'
-                    },
-                    {
-                        type: 'input',
-                        name: 'email',
-                        message: 'What is the email address of your team manager'
-                    },
-                    {
-                        type: 'input',
-                        name: 'office',
-                        message: 'What is your office number of your team manager'
-                    },
-                    {
-                        type: 'list',
-                        name: 'menu',
-                        message: 'Look at the menu below and choose an option',
-                        choices: ['add engineer', 'add intern', 'finish building my team' ]
-                    },
-                ])
-        }
-        else if (menuAnswers.menuTwo === 'add intern') {
-            return inquirer
-                .prompt([
-                    {
-                        type: 'input',
-                        name: 'internName',
-                        message: 'What is the name of your intern'
-                    },
-                    {
-                        type: 'input',
-                        name: 'internId',
-                        message: 'What is the ID number of your intern'
-                    },
-                    {
-                        type: 'input',
-                        name: 'internEmail',
-                        message: 'What is the email of your intern'
-                    },
-                    {
-                        type: 'input',
-                        name: 'internGithub',
-                        message: 'What is the github username for your intern'
-                    },
-                    {
-                        type: 'list',
-                        name: 'menu',
-                        message: 'Look at the menu below and choose an option',
-                        choices: ['add engineer', 'add intern', 'finish building my team' ]
-                    }
-                ])
-        }
-    })
+// create the funciton that initalizes the HTML file creation
+function teampage() {
+    inquirer.prompt(information)
+        .then(function(data) {
+            writeToFile('index.html', generatePage(data));
+            console.log(data);
+        })
+}
 
+// use callback to initalize the app
+teampage();
